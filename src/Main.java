b9994a2c0bbd3f279f25f320678e005bbdcdf64a
PabloViniegra <pablovpmadrid@gamil.com>
2021-02-04 17:16:57 +0100

    segundo ejercicio Cliente

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,46 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         DatagramSocket datagramSocket = null;
         InetAddress inetAddress;
+        File file = new File("hola.txt");
+        ByteArrayOutputStream byteArrayOutputStream;
+        FileInputStream fileInputStream;
         try {
             datagramSocket = new DatagramSocket();
             inetAddress = InetAddress.getLocalHost();
-            String userInput = "";
+            fileInputStream = new FileInputStream(file);
+            byteArrayOutputStream = new ByteArrayOutputStream();
 
             byte[] buffer;
-            while (!userInput.equals("*")) {
-                System.out.print("Introduce un texto:");
-                userInput = sc.nextLine();
-                buffer = userInput.getBytes(StandardCharsets.UTF_8);
-                DatagramPacket datagramPacket1 = new DatagramPacket(buffer, buffer.length,inetAddress,8080);
-                try {
-                    datagramSocket.send(datagramPacket1);
-                } catch (IOException e) {
-                    e.printStackTrace();
+
+            byte[] buf = new byte[1024];
+            try {
+                for (int readNum; (readNum = fileInputStream.read(buf)) != -1;) {
+                    byteArrayOutputStream.write(buf, 0, readNum); //no doubt here is 0
+                    //Writes len bytes from the specified byte array starting at offset off to this byte array output stream.
+                    System.out.println("read " + readNum + " bytes,");
                 }
+            } catch (IOException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } catch (SocketException | UnknownHostException e) {
+
+            buffer = byteArrayOutputStream.toByteArray();
+            DatagramPacket datagramPacket1 = new DatagramPacket(buffer, buffer.length, inetAddress, 8080);
+            try {
+                datagramSocket.send(datagramPacket1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        } catch (SocketException | UnknownHostException | FileNotFoundException e) {
             e.printStackTrace();
         }
         assert datagramSocket != null;
